2dabb6e435419c394620177e7c78f0107e0f145b
Jim Beck <jebeck@zoominternet.net>
2026-01-28 15:11:05 -0500

    Added more comments

--- Singleton/Singleton/ThreadSafeBuffer.cpp
+++ Singleton/Singleton/ThreadSafeBuffer.cpp
@@ -14,9 +14,14 @@ ThreadSafeBuffer& ThreadSafeBuffer::getInstance()
 
 void ThreadSafeBuffer::push(std::string& name, std::string item)
 {
+	// Wait for a slot
 	m_slotsSemaphore.acquire();
+
+	// Get exclusive access to the buffer and push the item
 	std::lock_guard<std::mutex> guard(m_mutex);
 	m_buffer.push(item);
+
+	// Signal an item
 	m_itemsSemaphore.release();
 
 	std::cout << "\"" << name << "\" cooked item \"" << item << "\"" << std::endl;
@@ -24,10 +29,15 @@ void ThreadSafeBuffer::push(std::string& name, std::string item)
 
 std::string ThreadSafeBuffer::pop(std::string& name)
 {
+	// Wait for an item
 	m_itemsSemaphore.acquire();
+
+	// Get exclusive access to the buffer and pop the item
 	std::lock_guard<std::mutex> guard(m_mutex);
 	std::string item = m_buffer.front();
 	m_buffer.pop();
+
+	// Signal a slot
 	m_slotsSemaphore.release();
 
 	std::cout << "   \"" << name << "\" ate item \"" << item << "\"" << std::endl;
